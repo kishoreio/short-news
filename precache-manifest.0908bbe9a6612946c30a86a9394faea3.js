96cdb7375ebd0e01b213e3975578d0a57f71258b
kishore S <kishore@kishores-MacBook-Pro.local>
2019-10-12 15:47:08 +0530

    Updates

---
+++ precache-manifest.0908bbe9a6612946c30a86a9394faea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d758f6e06ca6be5bf2b95877e375bcb2",
+    "url": "/short-news/index.html"
+  },
+  {
+    "revision": "641ca4933c9aebd1a260",
+    "url": "/short-news/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "641ca4933c9aebd1a260",
+    "url": "/short-news/static/js/2.0a5bf88c.chunk.js"
+  },
+  {
+    "revision": "72a28d22cda6c937e2e9",
+    "url": "/short-news/static/js/main.d05bf32a.chunk.js"
+  },
+  {
+    "revision": "476278f39a0310f93249",
+    "url": "/short-news/static/js/runtime-main.61486c28.js"
+  }
+]);
