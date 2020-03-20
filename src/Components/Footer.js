85807e6c8528c26784edfeaf87b5df206687f1fe
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-03-20 22:53:36 +0530

    updated footer content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return(
         <div>
             <footer class="page-footer font-small blue">
-                <div class="footer-copyright text-center py-3">Made with &#10084; by Kishore.
+                <div class="footer-copyright text-center py-3">Made with &#10084; by Kishore.io.
                 <a href="https://github.com/kishorecodes/short-news-react"> Github</a>
                 <a href="https://twitter.com/kishorecodes"> Twitter</a>
                 </div>
